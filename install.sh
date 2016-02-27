daae43c318ec7ef95de68218ad3202587deabed1
Ben <ben@auxsend.org>
2016-02-27 15:25:45 +0100

    upd

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 
+cwd=$(pwd)
+
+aux=$HOME/aux
 dev=$HOME/dev
+[ -d "$aux" ] || mkdir -p $aux
 [ -d "$dev" ] || mkdir -p $dev
+[ -e "$dev/aux" ] || ln -s $cwd $dev/aux
 
-rm -f $dev/opt
-ln -s $(pwd) $dev/opt
 
-rm -f $HOME/opt
-ln -s $(pwd) $HOME/opt
+rm -f $aux/opt
+ln -s $cwd $aux/opt
 
